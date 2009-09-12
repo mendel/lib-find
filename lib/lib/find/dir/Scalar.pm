ed231586a6eee20559167c9658de6b518856ff1d
Norbert Buchmuller <norbi@nix.hu>
2009-09-12 15:25:59 +0200

    lib::find::dir::Scalar does not use Tie::StdScalar any more.

--- lib/lib/find/dir/Scalar.pm
+++ lib/lib/find/dir/Scalar.pm
@@ -3,12 +3,45 @@ package lib::find::dir::Scalar;
 use warnings;
 use strict;
 
-use Tie::Scalar;
-use base qw(Tie::StdScalar);
+use Carp;
 
 =head1 NAME
 
 lib::find::dir::Scalar - lib::find internals - implement $lib::find::dir tied scalar
+
+=cut
+
+=head1 FUNCTIONS
+
+=cut
+
+sub _modify_scalar
+{
+  my ($self) = (shift, @_);
+
+  croak "You cannot modify the $self->{var_name} variable";
+}
+
+=head2 TIESCALAR
+
+See L<perltie/TIESCALAR>.
+
+=cut
+
+sub TIESCALAR
+{
+  my ($class, $var_name) = (shift, @_);
+
+  my $self = bless {}, $class;
+
+  $self->{var_name} = $var_name;
+
+  return $self;
+}
+
+=head2 FETCH
+
+See L<perltie/FETCH>.
 
 =cut
 
@@ -19,11 +52,7 @@ sub FETCH
   return $lib::find::dir{caller()};
 }
 
-sub STORE
-{
-  my ($self, $value) = (shift, @_);
+*STORE = \&_modify_scalar;
+*UNTIE = \&_modify_scalar;
 
-  return $lib::find::dir{caller()} = $value;
-}
-
-1; # End of lib::find::dir
+1; # End of lib::find::dir::Scalar
