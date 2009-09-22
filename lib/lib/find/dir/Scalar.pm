5929af4fd04890246be1d08112f1d78b2f2f6598
Norbert Buchmuller <norbi@nix.hu>
2009-09-22 21:51:16 +0200

    Fixed a bug in the $lib::find::dir implementation.
    Now it skips Path::Class::Entity (and subclasses) in the caller list.

--- lib/lib/find/dir/Scalar.pm
+++ lib/lib/find/dir/Scalar.pm
@@ -49,7 +49,16 @@ sub FETCH
 {
   my ($self) = (shift, @_);
 
-  return $lib::find::dir{caller()};
+  # find the first caller that is not Path::Class (often the actual fetch is
+  # happening from the $path_class->stringify call)
+  my $caller;
+  my $level = 0;
+  do {
+    $caller = caller($level++);
+  } while ($caller &&
+           $caller->can('isa') && $caller->isa('Path::Class::Entity'));
+
+  return $lib::find::dir{$caller};
 }
 
 *STORE = \&_modify_scalar;
